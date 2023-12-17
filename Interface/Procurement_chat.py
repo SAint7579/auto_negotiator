61a32b17e465eb09b9ca60f4bc30829f55d91c4b
anirudh <anirudhsai027@gmail.com>
2023-12-17 09:05:11 +0100

    6969696969696969

--- Interface/Procurement_chat.py
+++ Interface/Procurement_chat.py
@@ -3,21 +3,20 @@ from openai import OpenAI
 import json
 import sys
 sys.path.append('C:/VS code projects/Road to Hack/auto_negotiator/Utilities/')
-from request_functions import show_json
-from request_functions import submit_message
-from request_functions import get_response
-from request_functions import pretty_print
-from request_functions import wait_on_run
-from request_functions import check_response
-from request_functions import function_json
-from request_functions import MATH_ASSISTANT_ID
-from request_functions import thread
-from request_functions import client
+from negotiation_utils import *
+from negotiation_utils import gpt_draft_mail
+from negotiation_utils import submit_message
+from negotiation_utils import get_response
+from negotiation_utils import pretty_print
+from negotiation_utils import wait_on_run
+from negotiation_utils import check_response
+from negotiation_utils import MATH_ASSISTANT_ID
+from negotiation_utils import thread
+from negotiation_utils import client
 print(thread.id)
-st.title("Request Bot")
-
-requirements_path = 'C:/VS code projects/Road to Hack/auto_negotiator/Utilities/requirements.json'
-
+st.title("Procurement Bot")
+# Path to requirements file
+requirements_path = 'C:/VS code projects/Road to Hack/auto_negotiator/Utilities/procurement.json'
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -26,7 +25,17 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-        
+
+# Display initial response
+initialsiation = True
+if initialsiation:
+    specification = json.loads(json.load(open('C:/VS code projects/Road to Hack/auto_negotiator/Utilities/requirements.json','r')))
+    response = gpt_draft_mail(specification, 'ABC Company')
+    st.session_state.initialised = True
+
+    # Display assistant response
+    with st.chat_message("assistant"):
+        st.markdown(response)
 
 # React to user input
 if prompt := st.chat_input("What is up?"):
@@ -35,21 +44,20 @@ if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     
-    ## Send user message to assistant and display assistant response
-    run = submit_message(client,MATH_ASSISTANT_ID, thread, prompt)
-    run = wait_on_run(client,run, thread)
+    # Send user message to assistant and display assistant response
+    run = submit_message(client, MATH_ASSISTANT_ID, thread, prompt)
+    run = wait_on_run(client, run, thread)
     completion = False
     if run.status == 'requires_action':
         response,completion = check_response(client,thread,run)
     else:
         response = pretty_print(get_response(client,thread))
-
-    response = f"{response}"
+    
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         st.markdown(response)
-    # Add assistant response to chat history
     
+    # Add assistant response to chat history
     if completion is True:
         print('done')
         json_data = run.required_action.submit_tool_outputs.tool_calls[0].function.arguments
